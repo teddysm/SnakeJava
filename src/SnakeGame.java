31c6ac16938c07c763e5942a5e541eb9fa3ca095
Thanh Doan <doan.teddy@gmail.com>
2023-03-02 22:23:47 -0500

    add Snake gem

---
+++ src/SnakeGame.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Teddy Doan
+ *
+ */
+public class SnakeGame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new GameFrame();
+		
+	}
+
+}
